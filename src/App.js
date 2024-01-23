cec16aa60e93ebf22cf4ffccd7ab25afc8da61ad
yunseo <choijang1117@hanmail.net>
2024-01-23 20:49:20 +0900

    5일 차 커밋 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+    {
+        id: 1,
+        author: "최윤서",
+        content: "ㅎㅇ",
+        emotion: 1,
+        create_date: new Date().getDate(),
+    },
+    {
+        id: 2,
+        author: "안정인",
+        content: "먼작귀",
+        emotion: 2,
+        create_date: new Date().getTime(),
+    },
+    {
+        id: 3,
+        author: "김진석",
+        content: "좀비루팡",
+        emotion: 3,
+        create_date: new Date().getTime(),
+    },
+    {
+        id: 4,
+        author: "구도윤",
+        content: "대성 청정 가져가주세요.",
+        emotion: 4,
+        create_date: new Date().getTime(),
+    },
+];
 
 function App() {
   return (
     <div className="App">
         <DiaryEditor />
+        <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
