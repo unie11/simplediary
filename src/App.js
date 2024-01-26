da4fff8780ddaa2ca8d74228a033bde9ea87be46
yunseo <choijang1117@hanmail.net>
2024-01-26 15:14:29 +0900

    React에서 배열 사용하기 3 - 데이터 삭제하기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import {useState, useRef} from "react";
-
-// const dummyList = [
-//     {
-//         id: 1,
-//         author: "최윤서",
-//         content: "ㅎㅇ",
-//         emotion: 1,
-//         created_date: new Date().getTime(),
-//     },
-//     {
-//         id: 2,
-//         author: "안정인",
-//         content: "먼작귀",
-//         emotion: 2,
-//         created_date: new Date().getTime(),
-//     },
-//     {
-//         id: 3,
-//         author: "김진석",
-//         content: "좀비루팡",
-//         emotion: 3,
-//         created_date: new Date().getTime(),
-//     },
-//     {
-//         id: 4,
-//         author: "구도윤",
-//         content: "대성 청정 가져가주세요.",
-//         emotion: 4,
-//         created_date: new Date().getTime(),
-//     },
-// ];
+import {useRef, useState} from "react";
 
 function App() {
     const [data, setData] = useState([]);
@@ -52,12 +21,18 @@ function App() {
         setData([newItem, ...data]);
     };
 
+    const onDelete = (targetId) => {
+        console.log(`${targetId}가 삭제되었습니다`);
+        const newDiaryList = data.filter((it) => it.id !== targetId);
+        setData(newDiaryList);
+    };
+
     return (
-    <div className="App">
-        <DiaryEditor onCreate={onCreate}/>
-        <DiaryList diaryList={data}/>
-    </div>
-  );
+        <div className="App">
+            <DiaryEditor onCreate={onCreate}/>
+            <DiaryList onDelete={onDelete} diaryList={data}/>
+        </div>
+    );
 }
 
 export default App;
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,4 @@
-const DiaryItem = ({id, author, content, emotion, created_date}) => {
+const DiaryItem = ({onDelete, id, author, content, emotion, created_date}) => {
     return (
         <div className="DiaryItem">
             <div className="info">
@@ -7,6 +7,14 @@ const DiaryItem = ({id, author, content, emotion, created_date}) => {
                 <span className="date">{new Date(created_date).toLocaleString()}</span>
             </div>
             <div className="content">{content}</div>
+            <button onClick={() => {
+                console.log(id);
+                if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+                    onDelete(id);
+                }
+            }}>
+                삭제하기
+            </button>
         </div>
     );
 };
